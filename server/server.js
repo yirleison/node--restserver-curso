e2d650a65d6c707d3d91a49f40c820dd5c20f5bb
yirleison <yppalomeque@gmail.com>
2019-01-21 21:32:08 -0500

    primer commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('./config/config');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+
+let consola = console.log;
+ 
+app.get('/', function (req, res) {
+  res.send({"saludo": "Hola Yirleison"});
+})
+ 
+app.listen(process.env.PORT, ()=>{
+    consola('Escuchando en el puerto', 3000);
+})
