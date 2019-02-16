f95a455f64794d3e0fcff378b8b5fb26715256b6
yirleison <yppalomeque@gmail.com>
2019-02-16 18:47:05 -0500

    Se organiza de nuevo la proteccion de la ruta usuario para validar el rol

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ var app = express();
 let userController = require('../controller/user');
 const { verifiToken, verify_user_admin } = require('../middlewares/autentication');
 
-app.post('/usuario', userController.saveUser);
+app.post('/usuario',[verifiToken, verify_user_admin], userController.saveUser);
 app.put('/usuario/:id',[verifiToken, verify_user_admin], userController.updateUser);
 app.get('/usuario', [verifiToken, verify_user_admin ], userController.getUser);
 app.delete('/usuario/:id',[verifiToken, verify_user_admin], userController.userDelete);
